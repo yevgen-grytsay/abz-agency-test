c6de00f48ee3fcaa0a4d8f884a4b489e10884c79
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-03-09 14:31:43 +0200

    ci add zip

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git wget -yqq
+apt-get install git wget zip -yqq
 
 docker-php-ext-install pdo_mysql
 
