6187e8185537d8f024d13c878db3afb0aba5f2bf
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-03-09 14:12:34 +0200

    Add CI config for feature tests (draft)

---
+++ ci/docker_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# We need to install dependencies only for Docker
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+# Install git (the php image doesn't have it) which is required by composer
+apt-get update -yqq
+apt-get install git -yqq
+
+docker-php-ext-install pdo_mysql
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+composer install
+
+cp .env.example .env
+php artisan key:generate
