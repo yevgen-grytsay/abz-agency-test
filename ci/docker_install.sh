5b3bea4b78465aae57e30e82d4596595c8d2655d
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-03-09 14:25:12 +0200

    fix ci composer

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -11,10 +11,11 @@ apt-get install git -yqq
 
 docker-php-ext-install pdo_mysql
 
+wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
-php -r "unlink('composer-setup.php');"
+php -r "unlink('composer-setup.php'); unlink('installer.sig');"
 composer install
 
 cp .env.example .env
